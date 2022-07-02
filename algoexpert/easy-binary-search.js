186a264087b274cd23856822091ce53794ea8738
Obie <obiemunozjr@gmail.com>
2022-07-02 14:54:39 -0400

    Added recursive method

--- algoexpert/easy-binary-search.js
+++ algoexpert/easy-binary-search.js
@@ -12,3 +12,16 @@ function binarySearch(array, target) {
     }
     return resultIndex;
 }
+
+function binarySearchRecursive(array, target) {
+    return binarySearchRecursiveHelper(array, target, 0, array.length - 1)
+}
+
+function binarySearchRecursiveHelper(array, target, left, right) {
+    if (left > right) return -1;
+    let middle = Math.floor((left + right) / 2)
+    let potentialMatch = array[middle];
+    if (target == potentialMatch) return middle;
+    else if (target < potentialMatch) return binarySearchRecursiveHelper(array, target, left, middle - 1)
+    else if (target > potentialMatch) return binarySearchRecursiveHelper(array, target, middle + 1, right)
+}
