c86e540617f10c085981af9047c89d0b14cabccc
Obie <obiemunozjr@gmail.com>
2022-07-03 15:37:17 -0400

    LC: 125 - Valid Palindrome

---
+++ leetcode/125.valid-palindrome.js
@@ -0,0 +1,14 @@
+// constraints:
+//  1 <= s.length <= 2 * 10^5
+//  s consists of only printable ASCII characters
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    let t = s.replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+    let z = t.split('').reverse().join('').toLowerCase();
+    console.log(t, z)
+    return t == z
+};
